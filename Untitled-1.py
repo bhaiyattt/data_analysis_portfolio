cef90721783594c2b5498b9eda8ecba929a48843
Umar Bhaiyat <bhaiyatu@icloud.com>
2022-07-31 19:32:34 +0100

    messing around with new file commit

---
+++ Untitled-1.py
@@ -0,0 +1,10 @@
+
+list_of_names = ["Alex", "John", "Hades", "Marissa", "David"]
+
+def get_names(name):
+    if name in list_of_names:
+        return 
+    else:
+        print("Name is not in the list of names")
+
+get_names("Alex")
